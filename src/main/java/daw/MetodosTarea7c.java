169ac01312bfe6a991820b09c82c54921fc4e138
Daniel <daniel2004navas@gmail.com>
2024-03-21 23:26:30 +0100

    finalización tarea 7c

--- src/main/java/daw/MetodosTarea7c.java
+++ src/main/java/daw/MetodosTarea7c.java
@@ -8,12 +8,15 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  *
@@ -39,44 +42,13 @@ public class MetodosTarea7c {
 
     }
 
-    //método para pasar la lista de String a lista de personas
-    public static List<Persona> listaObjetoPersona() {
-        List<String> lineas = leerFichero();
-        //creo la lista que voy a devolver
-        List<Persona> persona = new ArrayList<>();
-        for (int i = 0; i < lineas.size(); i++) {
-            //separamos cada línea por , y lo guardamos en un array
-            String[] array = lineas.get(i).split(",");
-
-            //le damos un formato a la fecha de (día-mes-año)
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-            //creamos la fecha, le pasamos el string de la fecha y el formatter
-            //creado anterior
-            LocalDate fecha = LocalDate.parse(array[5], formatter);
-
-            //creamos una persona y cada vez que itere este for cambiarán
-            //los datos
-            Persona per = new Persona(Integer.parseInt(array[0]),
-                    array[1], array[2], array[3], array[4], fecha,
-                    Boolean.parseBoolean(array[6]), array[7]);
-            //añadimos a la lista la persona creada anteriormente
-            //(esta irá modificandose en cada iteración)
-            persona.add(per);
+    public static void escribir(String escribir, String idFichero) {
+        try {
+            Files.write(Paths.get(idFichero), (escribir + "\n").getBytes(StandardCharsets.UTF_8),
+                    StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.APPEND);
+        } catch (IOException ex) {
+            System.out.println("Error creando el fichero");
         }
-        return persona;
     }
 
-    //método que devuelve en un conjunto todos los tipos de datos de género
-    public static void conjuntoGenero(List<Persona> lista) {
-        //creamos el map para ir añadiendo el id y el género
-        Map<Integer,String> generoLinea = new HashMap<>();
-        
-        //recorremos la lista de personas
-        for (int i = 0; i < lista.size(); i++) {
-            //cada iteración se añade al Map el id y el género (aunque se repita
-            //el género el id nunca se repetirá)
-            generoLinea.put(lista.get(i).id(), lista.get(i).genero());
-        }
-        
-    }
 }
--- src/main/java/daw/Tarea7cDanielNavasBorjas.java
+++ src/main/java/daw/Tarea7cDanielNavasBorjas.java
@@ -1,8 +1,18 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package daw;
+
+import static daw.MetodosTarea7c.escribir;
+import static daw.MetodosTarea7c.leerFichero;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  *
@@ -11,8 +21,90 @@ package daw;
 public class Tarea7cDanielNavasBorjas {
 
     public static void main(String[] args) {
-        
+
         //mostramos los datos de la lista de persona por consola
-        MetodosTarea7c.listaObjetoPersona().forEach(System.out::println);
+        listaObjetoPersona().forEach(System.out::println);
+        
+        System.out.println("\nSiguiente método\n");
+        
+        //método que nos enseña por consola los géneros que hay
+        conjuntoGenero(listaObjetoPersona());
+        
+        System.out.println("\nSiguiente método\n");
+        
+        //método para contar géneros y guardarlo en un fichero .csv
+        contadorGeneros(listaObjetoPersona());
+    }
+
+    //método para pasar la lista de String a lista de personas
+    public static List<Persona> listaObjetoPersona() {
+        List<String> lineas = leerFichero();
+        //creo la lista que voy a devolver
+        List<Persona> persona = new ArrayList<>();
+        for (int i = 0; i < lineas.size(); i++) {
+            //separamos cada línea por , y lo guardamos en un array
+            String[] array = lineas.get(i).split(",");
+
+            //le damos un formato a la fecha de (día-mes-año)
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+            //creamos la fecha, le pasamos el string de la fecha y el formatter
+            //creado anterior
+            LocalDate fecha = LocalDate.parse(array[5], formatter);
+
+            //creamos una persona y cada vez que itere este for cambiarán
+            //los datos
+            Persona per = new Persona(Integer.parseInt(array[0]),
+                    array[1], array[2], array[3], array[4], fecha,
+                    Boolean.parseBoolean(array[6]), array[7]);
+            //añadimos a la lista la persona creada anteriormente
+            //(esta irá modificandose en cada iteración)
+            persona.add(per);
+        }
+        return persona;
+    }
+
+    //método que muestra los géneros que hay y escribe en un fichero cada género
+    public static void conjuntoGenero(List<Persona> lista) {
+        //creamos el map para ir añadiendo el id y el género
+        Set<String> set = new HashSet<>();
+
+        //recorremos la lista de personas
+        for (int i = 0; i < lista.size(); i++) {
+            //cada iteración se añade al set el género
+            set.add(lista.get(i).genero());
+
+            //aprovechamos este for para escribir en el fichero los géneros
+            escribir(lista.get(i).genero(), "generos.txt");
+        }
+
+        //mostramos por consola los géneros que hay en el set
+        for (String string : set) {
+            System.out.println(string);
+        }
+
+    }
+
+    //método para contar cuantos géneros hay
+    public static void contadorGeneros(List<Persona> lista) {
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < lista.size(); i++) {
+            if (map.containsKey(lista.get(i).genero())) {
+                map.put(lista.get(i).genero(), map.get(lista.get(i).genero()) + 1);
+            } else {
+                map.put(lista.get(i).genero(), 1);
+            }
+        }
+        
+        //hacemos un for eache para ir recorriendo e ir escribiendo en el
+        //fichero "contadorGeneros.csv"
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            //mostramos por consola
+            System.out.println(entry.getKey() + "," + entry.getValue());
+            //llamamos al método escribir para ir escribiendo como nos dice
+            //el examen
+            escribir(entry.getKey() + "," + entry.getValue(), "contadorGeneros.csv");
+            
+        }
+
     }
 }
